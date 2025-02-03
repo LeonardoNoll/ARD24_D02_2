cf490e6babdd260a62332a570acff008eb9ba917
Cristian Doring <cristiandoring@gmail.com>
2025-02-03 20:53:54 -0300

    feat: create Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-
+import Header from '../src/Components/Header'
 function App() {
   return (
     <>
+    <Header isLogin={true}/>
     </>
   );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,32 @@
+import logoGreen from '../assets/image/logo-green.svg'
+
+function Header(props: {isLogin: boolean}){
+    return(
+        <header className='w-[1440] h-[89px] border-b-1 flex justify-between py-4 px-10 border-slate-200 items-center'>
+            <img src={logoGreen} alt="planta" className='w-[49px] h-[54px] flex gap-[55px] '/>
+            {/*exibe menu diferente se estiver logado ou não*/}
+            {props.isLogin ? 
+            <nav className='w-[289px] h-[57px] p-4 flex gap-4'>
+                <a href='#' className='w-[58px] h-[25px] font-normal font400 leading-[24px] text-4 text-center text-slate-500'>Home</a>
+                <a href='#'className='w-[88px] h-[25px] font-normal font400 leading-[24px] text-4 text-center text-slate-500'>Products</a>
+                <a href='#'className='w-[79px] h-[25px] font-normal font400 leading-[24px] text-4 text-center text-slate-500'>About me</a>
+            </nav>
+            : <nav className='w-[90px] h-[57px] p-4 flex gap-4'>
+                <a href='#' className='w-[58px] h-[25px] font-normal font400 leading-[24px] text-4 text-center text-slate-500'>Home</a>
+            </nav> }
+            
+            {/*mostra um determinado botao se tiver ou não logado*/}
+                {props.isLogin ? 
+                <button className='w-[139px] h-12 flex gap-10 bg-emerald-900 rounded-[8px] py-3 px-10 font-inter font-[600] text-4 leading-[24.72px] text-center text-white'>Log out</button>
+                :   
+                <section className='w-[227px] h-12 flex gap-10 items-center' >
+                    <a className='w-16 h-[25px] font-inter font-[500] text-[16px] leading-[24px] text-center text-slate-900'>Register</a>
+                    <button className='w-[123px] h-12 flex gap-10 bg-emerald-900 rounded-[8px] py-3 px-10 font-inter font-[600] text-4 leading-[24.72px] text-center text-white'>Login</button>
+                </section>
+                }
+
+        </header>
+    )
+}
+
+export default Header
