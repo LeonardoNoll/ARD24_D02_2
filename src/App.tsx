001b2c8db735cc020af968b44c75423cd21d25da
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-02-04 16:13:43 -0400

    Criação de 50% da página home logoff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Header from '../src/Components/Header'
-import Footer from './Components/Footer';
+import Footer from '../src/Components/Footer';
+import Hero from './Components/Hero';
+import StepsToTake from './Components/StepsToTake';
 function App() {
   return (
     <>
     <Header isLogin={false}/>
+    <Hero />
+    <StepsToTake />
+
     
     <Footer/>
     
---
+++ src/pages/HomeLogoff.tsx
@@ -0,0 +1,10 @@
+import Hero from '../Components/Hero.tsx';
+import StepsToTake from '../Components/StepsToTake.tsx';
+export function homeLogoff() {
+    return (
+      <>
+        <Hero />
+        <StepsToTake />
+      </>
+    );
+  }
