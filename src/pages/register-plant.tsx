2ab4e4dbfd486752054ce116948b084d0c3cdf6f
Leonardo Noll Cruz <leonoll8@hotmail.com>
2025-02-04 01:48:19 +0000

    feat: :construction: started work on plant-register

--- src/pages/register-plant.tsx
+++ src/pages/register-plant.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
 const RegisterPlant = () => {
-  return (
-    <div>RegisterPlant</div>
-  )
+    return (
+        <main className="flex w-screen border justify-center">
+            <form action="" className="flex flex-col flex-1 p-12">
+                <section className="border">
+                    <h1 className="font-play-display text-5xl font-bold mb-3">Register Plant</h1>
+                    <h3>Lorem Ipsum</h3>
+                </section>
+                <div className="flex flex-col mb-6 gap-2">
+                    <label htmlFor="plant-name" className="font-inter font-md">Plant Name</label>
+                    <input type="text" name="plant-name" id="plant-name" placeholder="Echinocereus Cactus" className="bg-slate-100 rounded-sm border-1 border-slate-200 p-2" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            <aside className="border flex-1">
+                <img src="../assets/image/plant1.png" alt="imagem de uma planta em um vaso" />
+            </aside>
+        </main>
+    )
 }
 
 export default RegisterPlant
