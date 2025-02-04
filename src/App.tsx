a4ce986367c5d0b26d1d254807630a70d3bd6395
felpscirne <cirnefelipe@gmail.com>
2025-02-03 21:39:26 -0300

    wip: products page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from 'react';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
+
+
 
 function App() {
   return (
