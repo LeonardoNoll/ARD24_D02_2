878ba8b425fe9e5b7af154b6ad39b8b629fbdac0
Leonardo Noll <leonoll8@hotmail.com>
2025-02-05 16:28:52 -0300

    fix: routes quick fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import App from './App.tsx'
-import './index.css'
-import RegisterPlant from './pages/register-plant.tsx'
-import Products from './pages/Products.tsx'
-import { ProductProvider } from './context/ProductContext.tsx'
-import Header from './components/Header.tsx'
-import Footer from './components/Footer.tsx'
-
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
+import { ProductProvider } from "./context/ProductContext.tsx";
+import Header from "./Components/Header.tsx";
+import Footer from "./Components/Footer.tsx";
+import EditPlant from "./pages/EditPlant.tsx";
+import UserConfig from "./pages/UserConfig.tsx";
+import Products from "./pages/products.tsx";
+import RegisterPlant from "./pages/RegisterPlant.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ProductProvider>
-      <Routes>
-        <Route index element={<App />} />
-{/*        <Route path="register-plant" element={<RegisterPlant />} />
-        <Route path="edit-plant" element={<EditPlant />} />
-        <Route path="user-config" element={<UserConfig />} />*/}
-      <Route path='register-plant' element={<RegisterPlant/>}/>
-      <Route path='products' element={
-        <><Header isLogin={false} /><Products /><Footer/></>
-        }/>
-      </Routes>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="register-plant" element={<RegisterPlant />} />
+          <Route path="edit-plant" element={<EditPlant />} />
+          <Route path="user-config" element={<UserConfig />} />
+          <Route path="register-plant" element={<RegisterPlant />} />
+          <Route
+            path="products"
+            element={
+              <>
+                <Header isLogin={false} />
+                <Products />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
       </ProductProvider>
     </BrowserRouter>
   </StrictMode>,
