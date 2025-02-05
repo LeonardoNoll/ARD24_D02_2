e4ad9b990fa5860a14ed1f612a6ede8b988705de
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-02-05 13:43:48 -0400

    Finalização do layout. Faltando carrossel

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import Header from '../src/Components/Header'
 import Footer from '../src/Components/Footer';
-import Hero from './Components/Hero';
-import StepsToTake from './Components/StepsToTake';
-import Panel1 from './Components/Panel1';
-import BestSelling from './Components/BestSelling';
-import Carousel from './Components/Carousel';
 
 function App() {
   return (
     <>
     <Header isLogin={false}/>
-    <Hero />
-    <StepsToTake />
-    <Panel1 />
-    <BestSelling />
-    <Carousel />
-
     
     <Footer/>
     
-    {/* <Header isLogin={false}/>
-    
-    <Footer/> */}
     </>
   );
 }
