a129f0873d53e1da22f60338f0e17173f9b89ed1
Cristian Doring <cristiandoring@gmail.com>
2025-02-06 17:10:49 -0300

    feat: registration form validation

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import { HomeLogoff } from "./pages/HomeLogoff.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <App/>{/*
     <BrowserRouter>
       <ProductProvider>
         <Routes>
@@ -44,6 +45,6 @@ createRoot(document.getElementById("root")!).render(
           />
         </Routes>
       </ProductProvider>
-    </BrowserRouter>
+    </BrowserRouter>*/}
   </StrictMode>,
 );
