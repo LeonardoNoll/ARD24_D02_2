12c111e0c1cdabdd6bcd6e92368fce2aa39815c0
felpscirne <cirnefelipe@gmail.com>
2025-02-09 18:46:21 -0300

    Feat: PlantForm

---
+++ src/hooks/useFetchProduct.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../context/ProductContext';
+
+const useFetchProduct = (id: string) => {
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/products?id=${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch product');
+        }
+        const data = await response.json();
+        if (data.length > 0) {
+          setProduct(data[0]); // Acessa o primeiro elemento do array
+        } else {
+          throw new Error('Product not found');
+        }
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  return { product, loading, error };
+};
+
+export default useFetchProduct;
--- src/pages/AboutProduct.tsx
+++ src/pages/AboutProduct.tsx
@@ -1,38 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Product } from '../context/ProductContext';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import SidePlant from '../Components/SidePlant';
+import useFetchProduct from '../hooks/useFetchProduct';
 
 const AboutProduct: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const [product, setProduct] = useState<Product | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const response = await fetch(`http://localhost:3001/products?id=${id}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch product');
-        }
-        const data = await response.json();
-        if (data.length > 0) {
-          setProduct(data[0]); // Acessa o primeiro elemento do array
-        } else {
-          throw new Error('Product not found');
-        }
-      } catch (error: any) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProduct();
-  }, [id]);
+  const { product, loading, error } = useFetchProduct(id!);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -89,7 +64,7 @@ const AboutProduct: React.FC = () => {
             </div>
             <div className="mb-4">
             <button
-              className="bg-emerald-900 w-full h-12 rounded-[8px] px-10 py-3 font-inter text-white font-semibold text-center text-[16px] leading-6 mt-8"
+              className="bg-emerald-900 w-full h-12 rounded-[8px] px-10 py-3 font-inter text-white font-semibold text-center text-[16px] leading-6 mt-8 hover:bg-emerald-700 cursor-pointer"
               type="button"
               onClick={() => window.location.href = `${window.location.pathname}/edit`}
             >Edit Plant</button>
