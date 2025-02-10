8b08ba6a9bad711f0c6a4b283843df3827fd72e2
Leonardo Noll <leonoll8@hotmail.com>
2025-02-10 15:43:11 -0300

    fix: links to/from 403

--- src/Components/PrivateRoute.tsx
+++ src/Components/PrivateRoute.tsx
@@ -1,11 +1,11 @@
-import { RedirectToSignIn, useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
 import { Outlet } from "react-router-dom";
-import Login from "../pages/Login";
+import { Error403 } from "../pages/Error403";
 
 export default function PrivateRoute() {
   const { isSignedIn } = useUser();
 
   if (isSignedIn === undefined) return <div>Loading...</div>;
 
-  return isSignedIn ? <Outlet /> : <Login />;
+  return isSignedIn ? <Outlet /> : <Error403 />;
 }
